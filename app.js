198fb4f9b0db28d03ee9ba5b75f9c3f153940ed1
Lakshamana <lakshamana@protonmail.com>
2023-09-12 17:59:51 -0300

    feat: add user detail

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(cors())
 app.listen(process.env.PORT || 3000)
 
 app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')) })
+app.get('/user-detail', (req, res) => { res.sendFile(path.join(__dirname, 'user-detail.html')) })
 
 app.get('/oauth', (req, res) => {
   const csrfState = Math.random().toString(36).substring(2)
@@ -33,7 +34,7 @@ app.get('/oauth', (req, res) => {
   res.redirect(url)
 })
 
-app.get('/redirect', bodyParser({ extended: false }), async (req, res) => {
+app.get('/redirect', bodyParser, async (req, res) => {
   const { code, error_description } = req.body
 
   console.log({ code, error_description })
@@ -42,8 +43,9 @@ app.get('/redirect', bodyParser({ extended: false }), async (req, res) => {
     return res.status(400).json({ error: error_description })
   }
 
+  let data
   try {
-    const { data } = await axios.post(
+    data = await axios.post(
       'https://open.tiktokapis.com/v2/oauth/token', {
         client_key: process.env.CLIENT_KEY,
         client_secret: process.env.CLIENT_SECRET,
@@ -51,11 +53,32 @@ app.get('/redirect', bodyParser({ extended: false }), async (req, res) => {
         code,
         redirect_uri: process.env.REDIRECT_URI
       })
+  } catch (error) {
+    return res.status(401).json({ error: error.message })
+  }
 
-    return res.status(200).json({
-      accessToken: data.access_token,
-      refreshToken: data.refresh_token
-    })
+  try {
+    const userDataResponse = await axios.get(
+      'https://open.tiktokapis.com/v2/user/info', {
+        params: {
+          fields: 'open_id,avatar_url,display_name,is_verified,profile_deep_link'
+        },
+        headers: {
+          Authorization: `Bearer ${data.access_token}`
+        }
+      }
+    )
+
+    const params = {
+      username: userDataResponse.data.open_id,
+      like_count: userDataResponse.data.like_count,
+      follower_count: userDataResponse.data.follower_count,
+      is_verified: userDataResponse.data.is_verified
+    }
+
+    res.redirect(`/user-detail?${qs.stringify(params)}`)
+
+    return res.status(200).json({ data: userDataResponse.data })
   } catch (error) {
     return res.status(400).json({ error: error.message })
   }
